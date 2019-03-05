370a6156c6dd8ca74bb80e37a8a6d12120c43fd0
uladzislaudach <vladzayac1@gmail.com>
2019-03-05 17:50:34 +0300

    Add download cars with file

--- src/Parking.java
+++ src/Parking.java
@@ -26,11 +26,24 @@ class Parking {
                 " Свободных мест : " + numberPlaces);
     }
 
+    public void addCar(ArrayList<Car> carListWithFile) {
+        for (Car car : carListWithFile) {
+            if (validationCar(car)) {
+                carList.add(car);
+                numberPlaces = numberPlaces - 1;
+                System.out.println(car.toString() + " приехал на стоянку" +
+                        " Свободных мест : " + numberPlaces);
+            } else
+                System.out.println(car.toString() + " не может посетить стоянку (нет мест, с прицепом, большой вес)." +
+                        " Свободных мест : " + numberPlaces);
+        }
+    }
+
     private boolean validationCar(Car car) {
         return !car.isTrailer() && car.getWeight() <= 3000 && numberPlaces > 0;
     }
 
-    public  void deleteAuto() {
+    public void deleteAuto() {
         if (carList.size() <= 0) {
             System.out.println("Нет авто на парковке");
         } else {
@@ -45,7 +58,7 @@ class Parking {
         }
     }
 
-    public  void numberOfWhiteCar() {
+    public void numberOfWhiteCar() {
         int count = 0;
         for (Car car : carList) {
             if (car.getColour() == Colour.WHITE) {
@@ -55,7 +68,7 @@ class Parking {
         System.out.println("Количество белых авто на парковке: " + count);
     }
 
-    public  void numberOfAutomatCar() {
+    public void numberOfAutomatCar() {
         int count = 0;
         for (Car car : carList) {
             if (car.getGearBox() == GearBox.AUTOMAT) {
@@ -65,7 +78,7 @@ class Parking {
         System.out.println("Количество авто с автоматической КПП на парковке: " + count);
     }
 
-    public  void sameAuto() {
+    public void sameAuto() {
         for (Car car : carList) {
             int count = 0;
             for (Car car1 : carList) {
@@ -78,11 +91,11 @@ class Parking {
         }
     }
 
-    public  void sortListAuto() {
+    public void sortListAuto() {
         carList.sort(Car::compareTo);
     }
 
-    public  void changeNumberPlaces(int a) {
+    public void changeNumberPlaces(int a) {
         numberPlaces = numberPlaces + a;
         System.out.println("Количество парковочных мест увеличено на " + a + " мест \n" +
                 "Свободных мест : " + numberPlaces);
